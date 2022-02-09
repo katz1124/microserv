0517787875253ba0d5b2b8e2099f3b2f152b431f
katz1124 <katz1124@hotmail.com>
2022-02-09 17:58:27 -0600

    correcciones

--- MicroServ/registry-service/src/test/java/academy/digitallab/store/registryservice/RegistryServiceApplicationTests.java
+++ MicroServ/registry-service/src/test/java/academy/digitallab/store/registryservice/RegistryServiceApplicationTests.java
@@ -2,6 +2,8 @@ package academy.digitallab.store.registryservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootTest
 class RegistryServiceApplicationTests {
